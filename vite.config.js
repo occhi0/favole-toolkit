f1403643d1bf122df38ba3da235240bac3d5bba2
occhi0 <35575152+occhi0@users.noreply.github.com>
2025-07-06 09:06:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/favole-toolkit/', // 👈 AGGIUNGI QUESTA RIGA
   plugins: [
     react(),
     VitePWA({
